174a6b63be27c8d8c492ca99526c891942d22868
akram khan <107624376+Akramkn@users.noreply.github.com>
2024-01-07 18:31:49 +0500

    react-app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">wristup</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+};
+
+export default App;
